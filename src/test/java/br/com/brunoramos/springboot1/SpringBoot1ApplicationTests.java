70918a6e9c1073b8c694a6a6c74eaa7d67d8f0c9
brunoramux <bruno.lemos@live.com>
2024-02-12 14:45:45 -0300

    First Commit

---
+++ src/test/java/br/com/brunoramos/springboot1/SpringBoot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunoramos.springboot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
